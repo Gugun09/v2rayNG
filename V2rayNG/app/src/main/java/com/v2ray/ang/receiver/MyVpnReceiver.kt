9032c1e97af9d68e7fe49da30c2d4bcee5621770
Gugun <nandakanaksasak@gmail.com>
2025-07-07 20:49:44 +0800

    Update Bot

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/receiver/MyVpnReceiver.kt
@@ -0,0 +1,35 @@
+package com.v2ray.ang.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.v2ray.ang.service.V2RayServiceManager
+
+class MyVpnReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val token = intent.getStringExtra("token")
+        if (token != "abc123") {
+            Log.w("MyVpnReceiver", "Token salah, akses ditolak.")
+            return
+        }
+
+        when (intent.action) {
+            "com.v2rayng.START_VPN" -> {
+                Log.i("MyVpnReceiver", "Trigger START_VPN")
+                V2RayServiceManager.startVService(context)
+            }
+            "com.v2rayng.STOP_VPN" -> {
+                Log.i("MyVpnReceiver", "Trigger STOP_VPN")
+                V2RayServiceManager.stopVService(context)
+            }
+            "com.v2rayng.RESTART_VPN" -> {
+                Log.i("MyVpnReceiver", "Trigger RESTART_VPN")
+                V2RayServiceManager.stopVService(context)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    V2RayServiceManager.startVService(context)
+                }, 2000) // delay 2 detik
+            }
+        }
+    }
+}
